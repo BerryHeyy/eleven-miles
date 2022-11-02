174b41e355b44ed9e35a6efc183ebf523842d1d6
BerryHeyy <berkaykaplanbk@hotmail.com>
2022-11-02 21:56:28 +0100

    Added device checks

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,54 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <cstring>
+#include <vector>
+
+const std::vector<const char*> validation_layers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+    const bool enable_validation_layers = false;
+#else
+    const bool enable_validation_layers = true;
+#endif
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
 GLFWwindow* window;
+VkInstance vk_instance;
+VkPhysicalDevice physical_device = VK_NULL_HANDLE;
+
+bool check_validation_layer_support()
+{
+    // Get available validation layers.
+    uint32_t layer_count;
+    vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+
+    std::vector<VkLayerProperties> available_layers(layer_count);
+    vkEnumerateInstanceLayerProperties(&layer_count, available_layers.data());
+
+    // Check if required validation layers are in available layers.
+    for (const char* layer_name : validation_layers)
+    {
+        bool layer_found = false;
+
+        for (const VkLayerProperties layer_properties : available_layers)
+        {
+            if (strcmp(layer_name, layer_properties.layerName) == 0)
+            {
+                layer_found = true;
+                break;
+            }
+        }
+
+        if (!layer_found) return false;
+    }
+
+    return true;
+}
 
 void init_window()
 {
@@ -20,9 +63,94 @@ void init_window()
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 }
 
+bool is_device_suitable(VkPhysicalDevice device)
+{
+    VkPhysicalDeviceProperties device_properties;
+    vkGetPhysicalDeviceProperties(device, &device_properties);
+
+    VkPhysicalDeviceFeatures device_features;
+    vkGetPhysicalDeviceFeatures(device, &device_features);
+
+    return true; // Any gpu will do.
+}
+
+void pick_physical_device()
+{
+    uint32_t device_count = 0;
+    vkEnumeratePhysicalDevices(vk_instance, &device_count, nullptr);
+
+    if (device_count == 0)
+    {
+        throw std::runtime_error("Failed to find GPUs with Vulkan support.");
+    }
+
+    std::vector<VkPhysicalDevice> devices(device_count);
+    vkEnumeratePhysicalDevices(vk_instance, &device_count, devices.data());
+
+    for (VkPhysicalDevice device : devices)
+    {
+        if (is_device_suitable(device))
+        {
+            physical_device = device;
+            break;
+        }
+    }
+
+    if (physical_device == VK_NULL_HANDLE)
+    {
+        throw std::runtime_error("Failed to find a suitable GPU.");
+    }
+}
+
+void create_vulkan_instance()
+{
+    if (enable_validation_layers && !check_validation_layer_support())
+    {
+        throw std::runtime_error("One or more requested validation layers do not exist.");
+    }
+
+    VkApplicationInfo appInfo {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Eleven Miles";
+    appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(0, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    // Setup extensions from the built in glfw function
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    // Setup validation layers
+    if (enable_validation_layers)
+    {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validation_layers.size());
+        createInfo.ppEnabledLayerNames = validation_layers.data();
+    }
+    else
+    {
+        createInfo.enabledLayerCount = 0;
+    }
+
+    // Create instance
+    if (vkCreateInstance(&createInfo, nullptr, &vk_instance) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed to create Vulkan instance");
+    }
+}
+
 void init_vulkan()
 {
-
+    create_vulkan_instance();
 }
 
 void start_main_loop()
@@ -35,6 +163,8 @@ void start_main_loop()
 
 void cleanup() 
 {
+    vkDestroyInstance(vk_instance, nullptr);
+
     glfwDestroyWindow(window);
 
     glfwTerminate();
