ad6b9907f062dbc141162afcae03325c91758c07
BerryHeyy <berkaykaplanbk@hotmail.com>
2022-11-05 17:06:01 +0100

    Added command pool and buffer

--- src/eleven-miles.cpp
+++ src/eleven-miles.cpp
@@ -51,6 +51,9 @@ VkPipeline graphics_pipeline;
 
 std::vector<VkFramebuffer> swap_chain_framebuffers;
 
+VkCommandPool command_pool;
+VkCommandBuffer command_buffer;
+
 void init_window()
 {
     glfwInit();
@@ -722,9 +725,94 @@ void create_framebuffers()
         framebuffer_info.height = swap_chain_extent.height;
         framebuffer_info.layers = 1;
 
-        if (vkCreateFramebuffer(device, &framebuffer_info, nullptr, &swap_chain_framebuffers[i]) != VK_SUCCESS) {
+        if (vkCreateFramebuffer(device, &framebuffer_info, nullptr, &swap_chain_framebuffers[i]) != VK_SUCCESS)
+        {
             throw std::runtime_error("Failed to create framebuffer!");
         }
+    }
+}
+
+/* #endregion */
+
+/* #region Command Pools */
+
+void record_command_buffer(VkCommandBuffer command_buffer, uint32_t image_index)
+{
+    VkCommandBufferBeginInfo begin_info{};
+    begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    begin_info.flags = 0; // Optional
+    begin_info.pInheritanceInfo = nullptr; // Optional
+
+    if (vkBeginCommandBuffer(command_buffer, &begin_info) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed to begin recording command buffer!");
+    }
+
+    // Start render pass
+    VkRenderPassBeginInfo render_pass_info {};
+    render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    render_pass_info.renderPass = render_pass;
+    render_pass_info.framebuffer = swap_chain_framebuffers[image_index];
+    render_pass_info.renderArea.offset = {0, 0};
+    render_pass_info.renderArea.extent = swap_chain_extent;
+
+    VkClearValue clear_color = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
+    render_pass_info.clearValueCount = 1;
+    render_pass_info.pClearValues = &clear_color;
+
+    vkCmdBeginRenderPass(command_buffer, &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
+    vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphics_pipeline);
+
+    VkViewport viewport{};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = static_cast<float>(swap_chain_extent.width);
+    viewport.height = static_cast<float>(swap_chain_extent.height);
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+    vkCmdSetViewport(command_buffer, 0, 1, &viewport);
+
+    VkRect2D scissor{};
+    scissor.offset = {0, 0};
+    scissor.extent = swap_chain_extent;
+    vkCmdSetScissor(command_buffer, 0, 1, &scissor);
+
+    vkCmdDraw(command_buffer, 3, 1, 0, 0);
+
+    vkCmdEndRenderPass(command_buffer);
+
+    if (vkEndCommandBuffer(command_buffer) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed to record command buffer!");
+    }
+}
+
+void create_command_pool()
+{
+    QueueFamilyIndices queue_family_indices = find_queue_families(physical_device);
+
+    VkCommandPoolCreateInfo pool_info {};
+    pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    pool_info.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    pool_info.queueFamilyIndex = queue_family_indices.graphics_family.value();
+
+    if (vkCreateCommandPool(device, &pool_info, nullptr, &command_pool) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed to create command pool!");
+    }
+}
+
+void create_command_buffer()
+{
+    VkCommandBufferAllocateInfo alloc_info {};
+    alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    alloc_info.commandPool = command_pool;
+    alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    alloc_info.commandBufferCount = 1;
+
+    if (vkAllocateCommandBuffers(device, &alloc_info, &command_buffer) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed to allocate command buffers!");
     }
 }
 
@@ -744,6 +832,9 @@ void init_vulkan()
     create_graphics_pipeline();
 
     create_framebuffers();
+
+    create_command_pool();
+    create_command_buffer();
 }
 
 void start_main_loop()
@@ -756,6 +847,7 @@ void start_main_loop()
 
 void cleanup() 
 {
+    vkDestroyCommandPool(device, command_pool, nullptr);
     for (VkFramebuffer framebuffer : swap_chain_framebuffers)
     {
         vkDestroyFramebuffer(device, framebuffer, nullptr);
